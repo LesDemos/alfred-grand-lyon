9cc1d433392155693ab434b03fd4ef75fe6c1260
absouss <absoussdev@gmail.com>
2017-04-16 15:53:26 +0200

    Fetch port from process environment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log('Listening on port ' + port);
