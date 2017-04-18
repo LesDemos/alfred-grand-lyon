60f308c15a3fa97a17a85b289474ffaa1a0bf3ad
Devdevlyo <devon.brunet@laposte.net>
2017-04-18 19:44:16 +0200

    Suppression de l'url de ES du code

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
 "use strict"
+const ES_URL = process.env.ES_URL;
 var elasticsearch = require('elasticsearch');
 
 var client = new elasticsearch.Client( {
   hosts: [
-    'https://4z53u4b5kg:o8hk04e2l8@first-cluster-5558791793.eu-west-1.bonsaisearch.net',
+    ES_URL,
   ]
 });
 
